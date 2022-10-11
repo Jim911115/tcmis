68aaccee6218e80149d3b3faf7eeeb6ad59402cb
Jim911115 <s1101695@gm.pu.edu.tw>
2022-10-11 11:48:51 +0800

    make it better

--- index.py
+++ index.py
@@ -19,7 +19,8 @@ def course():
 
 @app.route("/today")
 def today():
-    now = datetime.now()
+    tz = timezone(timedelta(hours=+8))
+    now = datetime.now(tz)
     return render_template("today.html", datetime = str(now))
 
 @app.route("/welcome", methods=["GET", "POST"])
